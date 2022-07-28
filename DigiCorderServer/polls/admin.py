67da75a458f4bbbcabfbdcbdfbd1d7a77c86b264
Connor <gmunster33@gmail.com>
2022-07-28 08:23:22 -0500

    Frontend and Testing learning

--- DigiCorderServer/polls/admin.py
+++ DigiCorderServer/polls/admin.py
@@ -4,7 +4,18 @@ from django.contrib import admin
 
 from .models import Question, Choice, T6
 
-admin.site.register(Question)
+#admin.site.register(Question) # default ordering of fields
+
+class QuestionAdmin(admin.ModelAdmin): 
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+#     fields = ['pub_date', 'question_text'] # custom ordering of fields
+admin.site.register(Question, QuestionAdmin)
+
+    
+
 admin.site.register(Choice)
 admin.site.register(T6)
 
--- DigiCorderServer/polls/views.py
+++ DigiCorderServer/polls/views.py
@@ -1,5 +1,83 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from audioop import reverse
+from re import template
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        #Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+        '-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+    
+#     #template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     #return HttpResponse(template.render(context, request)) 
+#     return render(request, 'polls/index.html', context) #cleaner...
+
+# def detail(request, question_id):
+#     # return HttpResponse("You're looking at question %s." 
+#     # % question_id)
+
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question}) #cleaner
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save() #prevents race conditions - also offloads work to db
+        selected_choice.refresh_from_db() #prevents future calls from double incrementing on save
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
