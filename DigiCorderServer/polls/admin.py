d6ecedab99333bd73e2e7d7a5d7061d79443952f
Connor <gmunster33@gmail.com>
2022-07-28 12:08:34 -0500

    Admin Site Tweaks + Final Learning Push

--- DigiCorderServer/polls/admin.py
+++ DigiCorderServer/polls/admin.py
@@ -4,18 +4,25 @@ from django.contrib import admin
 
 from .models import Question, Choice, T6
 
+#admin.site.register(Choice) # default ordering/seperate form 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 #admin.site.register(Question) # default ordering of fields
 
 class QuestionAdmin(admin.ModelAdmin): 
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
 #     fields = ['pub_date', 'question_text'] # custom ordering of fields
 admin.site.register(Question, QuestionAdmin)
-
     
 
-admin.site.register(Choice)
 admin.site.register(T6)
 
--- DigiCorderServer/polls/models.py
+++ DigiCorderServer/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 import datetime
 
@@ -8,6 +9,12 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    @admin.display(
+            boolean=True,
+            ordering='pub_date',
+            description='Published recently?',
+        )
 
     def __str__(self):
         return self.question_text
