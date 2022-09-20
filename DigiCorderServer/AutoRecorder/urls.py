204416ebfb3fccfaa7b474a2909ed70c69ddea83
Connor <gmunster33@gmail.com>
2022-09-20 12:32:55 -0500

    Bug Fix

--- DigiCorderServer/AutoRecorder/urls.py
+++ DigiCorderServer/AutoRecorder/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     #path('', views.IndexView.as_view(), name='index'),
     path('', views.index, name='index'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('form355', views.form355, name='form355')
+    path('form355', views.form355, name='form355'),
+    path('dashboard/355/<str:tailNumber>', views.violation355View, name='violation355View'),
+    path('dashboard/edit/<str:tailNumber>', views.editView, name='editView')
+
 ]
